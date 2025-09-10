8299223a0c1c6325368a09b96decfeac04899011
Boris Besemer <borisbesemer@gmail.com>
2025-09-10 09:33:21 +0200

    feat: add pokeapi tool call with span

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,12 @@
-import { convertToModelMessages, streamText, type UIMessage } from "ai";
+import { SpanStatusCode, trace } from "@opentelemetry/api";
+import {
+	convertToModelMessages,
+	stepCountIs,
+	streamText,
+	tool,
+	type UIMessage,
+} from "ai";
+import z from "zod";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -8,6 +16,10 @@ export async function POST(req: Request) {
 
 	const result = streamText({
 		model: "openai/gpt-4o",
+		stopWhen: stepCountIs(10),
+		tools: {
+			pokemonInfo: pokemonInfoTool,
+		},
 		messages: convertToModelMessages(messages),
 		experimental_telemetry: {
 			isEnabled: true,
@@ -17,3 +29,49 @@ export async function POST(req: Request) {
 
 	return result.toUIMessageStreamResponse();
 }
+
+const pokemonInfoTool = tool({
+	name: "pokemonInfo",
+	description:
+		"Use the poke API to get information about a pokemon, this uses pokeapi.co/api/v2/ and ",
+	inputSchema: z.object({
+		path: z
+			.string()
+			.describe(
+				"The path to the pokemon information without a preceding slash, it gets added to https://pokeapi.co/api/v2/",
+			),
+	}),
+	execute: async ({ path }: { path: string }): Promise<string> => {
+		return trace
+			.getTracer("next-app")
+			.startActiveSpan("pokeapi.fetch", async (span) => {
+				const url = `https://pokeapi.co/api/v2/${path}`;
+				span.setAttribute("component", "pokemonInfoTool");
+				span.setAttribute("url.full", url);
+				span.setAttribute("http.method", "GET");
+				try {
+					const response = await fetch(url);
+					span.setAttribute("http.status_code", response.status);
+					const data = await response.json();
+					span.setStatus({ code: SpanStatusCode.OK });
+					return data as string;
+				} catch (error) {
+					if (error instanceof Error) {
+						span.recordException(error);
+						span.setStatus({
+							code: SpanStatusCode.ERROR,
+							message: error.message,
+						});
+					} else {
+						span.setStatus({
+							code: SpanStatusCode.ERROR,
+							message: "Unknown error",
+						});
+					}
+					return `Error: ${error instanceof Error ? error.toString() : "Unknown error"}`;
+				} finally {
+					span.end();
+				}
+			});
+	},
+});
